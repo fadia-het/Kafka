9cc21553f87c32cb80896cc51d5ddfeba2e0114b
fadia-het <107173679+fadia-het@users.noreply.github.com>
2022-07-17 00:53:44 +0530

    Update Producer.java

--- Kafka/Producer.java
+++ Kafka/Producer.java
@@ -20,7 +20,18 @@ public class Producer {
 
     public static void main(String[] args) {
 
-        String bootS
+        Properties p = new Properties();
+        p.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        p.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        p.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(p);
+
+        for(int i=0;i<10;i++){
+            ProducerRecord<String,String>record= new ProducerRecord<>("het","key+"+1,"value_"+1);
+            producer.send(record);
+        }
 
     }
 
